84ae27cf74c9695e347244fa2124fe24d8781066
syunk38 <syunk38@gmail.com>
2016-02-16 00:37:48 +0900

    modify

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module KuraCounter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.default_timezone = :local
   end
 end
